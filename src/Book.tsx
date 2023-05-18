789b71b3f430586b2677f72128c16d52a2af6f2e
JosiahFu <josiahfu@gmail.com>
2023-05-18 00:11:25 -0700

    Transition animations
    This took far longer than it should have

--- src/Book.tsx
+++ src/Book.tsx
@@ -1,8 +1,9 @@
-import { ReactNode, useContext } from "react";
+import { ReactNode, useContext, useRef } from "react";
 import { SelectedContext } from "./Bookshelf";
 import Portal from "./Portal";
+import { CSSTransition } from 'react-transition-group';
 
-function Book({children, coverImage, title, position = [0, 0, 0], color }: {
+function Book({ children, coverImage, title, position = [0, 0, 0], color }: {
     children: ReactNode,
     coverImage: string,
     title: string,
@@ -14,28 +15,39 @@ function Book({children, coverImage, title, position = [0, 0, 0], color }: {
     const handleClick = () => {
         setSelected(title);
     }
-    return (
-        <section className={'book'}>
-            <div className="cover" onClick={handleClick} style={{
-                transform: `translate(${position[0]}px, ${position[1]}px) rotate(${position[2]}deg)`,
-                backgroundColor: color,
-                visibility: opened ? 'hidden' : undefined
-            }}>
-                <img src={coverImage} alt={title} className="cover-image" />
-                <h2>{title}</h2>
+    const coverRef = useRef<HTMLDivElement>(null);
+    const contentRef = useRef<HTMLDivElement>(null);
+    const backgroundRef = useRef<HTMLDivElement>(null);
+    return (<>
+        <CSSTransition nodeRef={coverRef} in={!opened} timeout={1000} classNames="slide">
+            <div ref={coverRef}>
+                <div className="cover" onClick={handleClick} style={{
+                    transform: `translate(${position[0]}px, ${position[1]}px) rotate(${position[2]}deg)`,
+                    backgroundColor: color,
+                    // visibility: opened ? 'hidden' : undefined
+                }}>
+                    <img src={coverImage} alt={title} className="cover-image" />
+                    <h2>{title}</h2>
+                </div>
             </div>
-            {opened && <Portal>
-                <div className="background" onClick={() => setSelected(null)}></div>
-                <div className="popup-book-container" style={{ backgroundColor: color }}>
-                    <div className="popup-book">
-                        <h2>{title}</h2>
-                        <img src={coverImage} alt={title} className="cover-image" />
-                        {children}
+        </CSSTransition>
+        <Portal>
+            <CSSTransition nodeRef={backgroundRef} in={opened} timeout={1000} classNames="fade" appear unmountOnExit>
+                <div ref={backgroundRef} className="background" onClick={() => setSelected(null)}></div>
+            </CSSTransition>
+            <CSSTransition nodeRef={contentRef} in={opened} timeout={1000} classNames="slide" appear unmountOnExit>
+                <div ref={contentRef} className="content-container">
+                    <div className="content" style={{ backgroundColor: color }}>
+                        <div className="content-pages">
+                            <h2>{title}</h2>
+                            <img src={coverImage} alt={title} className="cover-image" />
+                            {children}
+                        </div>
                     </div>
                 </div>
-            </Portal>}
-        </section>
-    );
+            </CSSTransition>
+        </Portal>
+    </>);
 }
 
 export default Book;
--- src/Portal.tsx
+++ src/Portal.tsx
@@ -3,14 +3,8 @@ import { createPortal } from "react-dom";
 
 const Portal = ({children}: PropsWithChildren) => {
   const mount = document.getElementById("portal-root")!;
-  const el = document.createElement("div");
 
-  useEffect(() => {
-    mount.appendChild(el);
-    return () => {mount.removeChild(el)};
-  }, [el, mount]);
-
-  return createPortal(children, el)
+  return createPortal(children, mount)
 };
 
 export default Portal;
